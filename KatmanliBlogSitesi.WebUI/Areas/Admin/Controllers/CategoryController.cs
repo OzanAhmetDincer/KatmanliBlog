d64ec1ed0ab83638444c3066c187fef231e410b0
OzanAhmetDincer <ozanahmetdincer@gmail.com>
2023-01-07 22:05:12 +0300

    Add project files.

---
+++ KatmanliBlogSitesi.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,114 @@
+﻿using KatmanliBlogSitesi.Entites;
+using KatmanliBlogSitesi.Service.Abstract;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KatmanliBlogSitesi.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IService<Category> _service;
+   
+
+        public CategoryController(IService<Category> service)
+        {
+            _service = service;
+        }
+
+        // GET: CategoryController
+        public ActionResult Index()
+        {
+            var model = _service.GetAll();
+            return View(model);
+        }
+
+        // GET: CategoryController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CategoryController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CategoriesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateAsync(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    
+                    await _service.AddAsync(category);
+                    await _service.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata Oluştu!");
+                }
+            }
+            return View(category);
+        }
+
+        // GET: CategoryController/Edit/5
+        public async Task<ActionResult> EditAsync(int id)
+        {
+            var model = await _service.FindAsync(id);
+            return View(model);
+        }
+
+        // POST: CategoryController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditAsync(int id, Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    
+                    _service.Update(category);
+                    await _service.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata Oluştu!");
+                }
+            }
+            return View(category);
+        }
+
+        // GET: CategoryController/Delete/5
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            var model = await _service.FindAsync(id);
+            return View(model);
+        }
+
+        // POST: CategoryController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Category category)
+        {
+            try
+            {
+                _service.Delete(category);
+                _service.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
